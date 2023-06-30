13e7101f2cb4fba5d0dea8972a30033ea60da005
mango7431 <rsm5065@naver.com>
2023-06-30 17:58:32 +0900

    필요없는 prev next 삭제

--- finalAdmin/src/main/java/test/com/admin/vo/PageVO.java
+++ finalAdmin/src/main/java/test/com/admin/vo/PageVO.java
@@ -18,35 +18,34 @@ public class PageVO {
 	private int endPage;
 	private int totalPage;
 	
-	private boolean prev, next;
-	
 	public PageVO(int page,int size, int totalCount) {
 		this.size=size;
-		this.totalCount=totalCount;
+		this.totalCount=totalCount; // 총 데이터 갯수
 		
 		// 페이지 갯수
 		totalPage = (totalCount - 1)/size+1;
 		
-		//페이지가 잘못 넘어왔을때
+		// 페이지가 잘못 넘어왔을때
 		this.page = (page<1)?1:page;
 		this.page = (page>totalPage)? totalPage:page;
 		
+		// 1페이지면 start = 1 즉, 데이터 첫번째부터
 		start = (this.page-1)*size+1;
+		// 1페이지일때 start = 1이니 마지막은 10 즉, 1~10데이터
 		end = start + (size-1);
 		
 		// totalCount 이하의 값이어야 한다.
 		this.end = this.end>totalCount? totalCount:this.end;
 		
-		// page가 11~20일때 startpage는 모두 11
+		// page가 11~20일때 startpage는 모두 11 즉, 1~10 = 1 / 11~20 = 11
 		startPage=(this.page-1)/10*10+1;
 		
+		// startpage가 11일때 + 9 해서 20
 		endPage = startPage + 9;
 		
 		// totalPage 이하의 값이어야 한다.
 		this.endPage = this.endPage>totalPage? totalPage:this.endPage;
 		
-		prev = (page>1);
-		next = (page<totalPage);
 		
 	}
 
